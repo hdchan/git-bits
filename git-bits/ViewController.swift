87838529a711cd4046e4c6825bab5fa3db378e81
Henry Chan <henry.d.chan@gmail.com>
2019-04-28 10:56:45 -0400

    Finished developing "Hello World!" label

--- git-bits/ViewController.swift
+++ git-bits/ViewController.swift
@@ -9,7 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private lazy var helloWorldLabel: UILabel = {
+        let view = UILabel()
+        view.text = "Hello World!"
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var dateLabel: UILabel = {
+        let view = UILabel()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let date = formatter.string(from: Date())
+        view.text = date
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    override func loadView() {
+        super.loadView()
+        
+        view.addSubview(helloWorldLabel)
+        view.addSubview(dateLabel)
+        
+        NSLayoutConstraint.activate([
+            helloWorldLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            helloWorldLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            dateLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            dateLabel.topAnchor.constraint(equalTo: helloWorldLabel.bottomAnchor, constant: 10)
+            ])
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
